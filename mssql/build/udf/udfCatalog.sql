88cfadcdba4fb5729af84e5413ff94e0716d9f23
mdashkezari <demo@mit.edu>
2019-09-16 20:01:32 -0700

    udfCatalog update

--- mssql/build/udf/udfCatalog.sql
+++ mssql/build/udf/udfCatalog.sql
@@ -3,45 +3,58 @@ GO
 
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
 
 
 CREATE FUNCTION [dbo].[udfCatalog]()
 RETURNS TABLE
 AS
 RETURN (
-    SELECT RTRIM(LTRIM(Short_Name)) AS Variable, 
-    RTRIM(LTRIM(Long_Name)) AS [Long Name], 
-    RTRIM(LTRIM(Unit)) AS Unit, 
-    RTRIM(LTRIM(Make)) AS Make, 
-    RTRIM(LTRIM(Sensor)) AS Sensor, 
-    RTRIM(LTRIM(Process_Stage_Long)) AS [Process Level], 
-    RTRIM(LTRIM(Study_Domain)) AS [Study Domain], 
-    RTRIM(LTRIM(Temporal_Resolution)) AS [Temporal Resolution], 
-    RTRIM(LTRIM(Spatial_Resolution)) AS [Spatial Resolution], 
-    RTRIM(LTRIM(Comment)) AS [Comment], 
-    RTRIM(LTRIM(Dataset_Long_Name)) AS [Dataset Name], 
-    RTRIM(LTRIM(Data_Source)) AS [Data Source], 
-    RTRIM(LTRIM(Distributor)) AS [Distributor], 
-    RTRIM(LTRIM(Description)) AS [Dataset Description], 
-    [tblVariables].Dataset_ID AS [Dataset_ID], 
-    [tblVariables].ID AS [ID], 
-    [tblVariables].Table_Name AS [Table_Name], 
-    [tblVariables].Keywords AS [Keywords] 
-    FROM tblVariables 
-    JOIN tblDatasets ON [tblVariables].Dataset_ID=[tblDatasets].ID 
-    JOIN tblTemporal_Resolutions ON [tblVariables].Temporal_Res_ID=[tblTemporal_Resolutions].ID 
-    JOIN tblSpatial_Resolutions ON [tblVariables].Spatial_Res_ID=[tblSpatial_Resolutions].ID 
-    JOIN tblMakes ON [tblVariables].Make_ID=[tblMakes].ID 
-    JOIN tblSensors ON [tblVariables].Sensor_ID=[tblSensors].ID 
-    JOIN tblProcess_Stages ON [tblVariables].Process_ID=[tblProcess_Stages].ID 
-    JOIN tblStudy_Domains ON [tblVariables].Study_Domain_ID=[tblStudy_Domains].ID 
+SELECT RTRIM(LTRIM(Short_Name)) AS Variable,
+   [tblVariables].Table_Name AS [Table_Name],
+   RTRIM(LTRIM(Long_Name)) AS [Long_Name],
+   RTRIM(LTRIM(Unit)) AS [Unit],
+   RTRIM(LTRIM(Make)) AS [Make],
+   RTRIM(LTRIM(Sensor)) AS [Sensor],
+   RTRIM(LTRIM(Process_Stage_Long)) AS [Process_Level],
+   RTRIM(LTRIM(Study_Domain)) AS [Study_Domain],
+   RTRIM(LTRIM(Temporal_Resolution)) AS [Temporal_Resolution],
+   RTRIM(LTRIM(Spatial_Resolution)) AS [Spatial_Resolution],
+   JSON_VALUE(JSON_stats,'$.time.min') AS [Time_Min],
+   JSON_VALUE(JSON_stats,'$.time.max') AS [Time_Max],
+   JSON_VALUE(JSON_stats,'$.lat.min') AS [Lat_Min],
+   JSON_VALUE(JSON_stats,'$.lat.max') AS [Lat_Max],
+   JSON_VALUE(JSON_stats,'$.lon.min') AS [Lon_Min],
+   JSON_VALUE(JSON_stats,'$.lon.max') AS [Lon_Max],
+   JSON_VALUE(JSON_stats,'$.depth.min') AS [Depth_Min],
+   JSON_VALUE(JSON_stats,'$.depth.max') AS [Depth_Max],
+   JSON_VALUE(JSON_stats,'$."'+[Short_Name]+'".max') AS [Variable_25th],
+   JSON_VALUE(JSON_stats,'$."'+[Short_Name]+'"."50%"') AS [Variable_50th],
+   JSON_VALUE(JSON_stats,'$."'+[Short_Name]+'"."75%"') AS [Variable_75th],
+   JSON_VALUE(JSON_stats,'$."'+[Short_Name]+'".count') AS [Variable_Count],
+   JSON_VALUE(JSON_stats,'$."'+[Short_Name]+'".mean') AS [Variable_Mean],
+   JSON_VALUE(JSON_stats,'$."'+[Short_Name]+'".std') AS [Variable_Std],
+   JSON_VALUE(JSON_stats,'$."'+[Short_Name]+'".min') AS [Variable_Min],
+   JSON_VALUE(JSON_stats,'$."'+[Short_Name]+'".max') AS [Variable_Max],
+   RTRIM(LTRIM(Comment)) AS [Comment],
+   RTRIM(LTRIM(Dataset_Long_Name)) AS [Dataset_Name],
+   RTRIM(LTRIM(Data_Source)) AS [Data_Source],
+   RTRIM(LTRIM(Distributor)) AS [Distributor],
+   RTRIM(LTRIM(Description)) AS [Dataset_Description],
+   [tblVariables].Dataset_ID AS [Dataset_ID],
+   [tblVariables].ID AS [ID],
+   [keywords_agg].Keywords AS [Keywords]
+   FROM tblVariables
+   JOIN tblDataset_Stats ON [tblVariables].Table_Name = [tblDataset_Stats].Dataset_Name
+   JOIN tblDatasets ON [tblVariables].Dataset_ID=[tblDatasets].ID
+   JOIN tblTemporal_Resolutions ON [tblVariables].Temporal_Res_ID=[tblTemporal_Resolutions].ID
+   JOIN tblSpatial_Resolutions ON [tblVariables].Spatial_Res_ID=[tblSpatial_Resolutions].ID
+   JOIN tblMakes ON [tblVariables].Make_ID=[tblMakes].ID
+   JOIN tblSensors ON [tblVariables].Sensor_ID=[tblSensors].ID
+   JOIN tblProcess_Stages ON [tblVariables].Process_ID=[tblProcess_Stages].ID
+   JOIN tblStudy_Domains ON [tblVariables].Study_Domain_ID=[tblStudy_Domains].ID
+   JOIN (SELECT var_ID, STRING_AGG (keywords, ', ') AS Keywords FROM tblVariables var_table
+   JOIN tblKeywords key_table ON [var_table].ID = [key_table].var_ID GROUP BY var_ID)
+   AS keywords_agg ON [keywords_agg].var_ID = [tblVariables].ID
 )
-
-
-GO
-
-
