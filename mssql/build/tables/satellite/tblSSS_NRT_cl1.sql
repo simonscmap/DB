a54c75774852727565a47d9b5a0412be55482216
dcharing <dharing@uw.edu>
2023-04-26 13:12:28 -0700

    additional tables

---
+++ mssql/build/tables/satellite/tblSSS_NRT_cl1.sql
@@ -0,0 +1,69 @@
+USE [Opedia]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblSSS_NRT_cl1](
+	[time] [date] NOT NULL,
+	[lat] [float] NOT NULL,
+	[lon] [float] NOT NULL,
+	[sss_smap] [float] NULL,
+	[year] [smallint] NULL,
+	[month] [tinyint] NULL,
+	[week] [tinyint] NULL,
+	[dayofyear] [smallint] NULL
+) ON [FG5]
+GO
+
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_tblSSS_NRT_cl1_time_lat_lon] ON [dbo].[tblSSS_NRT_cl1]
+(
+	[time] ASC,
+	[lat] ASC,
+	[lon] ASC
+)
+INCLUDE ([sss_smap]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [FG5]
+GO
+
+CREATE NONCLUSTERED INDEX [IX_tblSSS_NRT_cl1_year_lat_lon] ON [dbo].[tblSSS_NRT_cl1]
+(
+	[year] ASC,
+	[lat] ASC,
+	[lon] ASC
+)
+INCLUDE ([sss_smap]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [FG5]
+GO
+
+CREATE NONCLUSTERED INDEX [IX_tblSSS_NRT_cl1_month_lat_lon] ON [dbo].[tblSSS_NRT_cl1]
+(
+	[month] ASC,
+	[lat] ASC,
+	[lon] ASC
+)
+INCLUDE ([sss_smap]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [FG5]
+GO
+
+CREATE NONCLUSTERED INDEX [IX_tblSSS_NRT_cl1_week_lat_lon] ON [dbo].[tblSSS_NRT_cl1]
+(
+	[week] ASC,
+	[lat] ASC,
+	[lon] ASC
+)
+INCLUDE ([sss_smap]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [FG5]
+GO
+
+
+CREATE NONCLUSTERED INDEX [IX_tblSSS_NRT_cl1_dayofyear_lat_lon] ON [dbo].[tblSSS_NRT_cl1]
+(
+	[dayofyear] ASC,
+	[lat] ASC,
+	[lon] ASC
+)
+INCLUDE ([sss_smap]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [FG5]
+GO
+
